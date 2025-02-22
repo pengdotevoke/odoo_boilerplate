7bf7b7515b62d7ceb1cda6586ef4931064b2b96b
James Oginga <otipapajim@gmail.com>
2025-02-22 17:53:05 +0300

    Added basic views structure and access rights

--- __manifest__.py
+++ __manifest__.py
@@ -2,13 +2,14 @@
     'name': 'Odoo Boilerplate',
     'version': '1.0',
     'category': 'Accounting',
-    'summary': 'CCustom module that creates basic templates for odoo app development',
+    'summary': 'Custom module that creates basic templates for odoo app development',
     'author': 'James Oginga',
     'depends': ['base'],  
-    'views': [
-        'views/hospital_patient_view.xml'
-        'views/menu.xml'
-        ],
+    'data': [
+    'security/ir.model.access.csv', 
+    'views/hospital_patient_view.xml',   
+    'views/menu.xml',
+    ],
     'installable': True,
     'application': True,
 }
